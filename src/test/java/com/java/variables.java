274949a7a7b40dbe02294157e0f3876ce7023502
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2022-03-31 08:03:10 +0530

    31March2022 java basics

--- src/test/java/com/java/variables.java
+++ src/test/java/com/java/variables.java
@@ -3,13 +3,13 @@ package com.java;
 public class variables {
 	
 	int i=10; //instance vairable
-	static int j=10; //static or class variable
-	
-	public int Add(int a , int b) {
+	static int j=10; //static or class variable : wehen ever class is loaded into memory static variables will get initiliaze at first 
+	boolean flag= true;
+	public void Add(int a , int b) {
 		//a,b,sum are local variables.
 		int sum = a+b; 
 		System.out.println("sum:"+sum);
-		return sum;
+		//return sum;
 		
 	}
 
@@ -18,7 +18,14 @@ public class variables {
 		System.out.println("insiede main method");
 		variables obj = new variables();
 		obj.Add(5,8);
-
+System.out.println(obj.i);
+System.out.println(j);
+System.out.println("---------------");
+System.out.println(j++);
+System.out.println(++j);
+System.out.println(~obj.i);
+System.out.println(!obj.flag);
+System.out.println(j>18?j:18);
 	}
 
 }
