1e54952fae933e662c4357c3a1d4442897b08e28
RaghuveerChebrolu <raghuveerchebrolu@gmail.com>
2022-05-10 11:24:41 +0530

    10 May 2022 Java OOPS Basics

---
+++ src/test/java/com/oopsBasics/finalClass.java
@@ -0,0 +1,20 @@
+package com.oopsBasics;
+
+//if a class is declared as final we cannot extend it
+final class Bike3121 {
+	void run() {
+		System.out.println("bike is running");
+	}
+}
+
+class finalClass // extends Bike3121
+{
+	void run() {
+		System.out.println("running safely with 100kmph");
+	}
+
+	public static void main(String args[]) {
+		finalClass honda = new finalClass();
+		honda.run();
+	}
+}
